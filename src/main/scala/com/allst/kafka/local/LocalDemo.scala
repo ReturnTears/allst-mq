f7bb8eb5ec31b04ff08058eae73b342ae453b95c
returntears <returntears@outlook.com>
2019-12-09 23:39:20 +0800

    kafka基本概念

---
+++ src/main/scala/com/allst/kafka/local/LocalDemo.scala
@@ -0,0 +1,11 @@
+package com.allst.kafka.local
+
+/**
+  * @author YiYa
+  * @since 2019-12-09 下午 11:38
+  */
+object LocalDemo {
+    def main(args: Array[String]): Unit = {
+        println("LocalDemo")
+    }
+}
---
+++ src/main/scala/com/allst/kafka/master/MasterDemo.scala
@@ -0,0 +1,11 @@
+package com.allst.kafka.master
+
+/**
+  * @author YiYa
+  * @since 2019-12-09 下午 11:38
+  */
+object MasterDemo {
+    def main(args: Array[String]): Unit = {
+        println("MasterDemo")
+    }
+}
